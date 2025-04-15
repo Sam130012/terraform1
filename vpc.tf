6f2b81cd92195516f4d58249470ea2407684568d
Sam130012 <samyash18012000@gmail.com>
2025-04-15 10:55:37 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -10,17 +10,17 @@ resource "aws_subnet" "public_subnet"{
     vpc_id = aws_vpc.my_vpc.id
     count = length(var.public_cidr_block)
     cidr_block = element(var.public_cidr_block, count.index)
-    avalability_zone = element(var.avalability_zone, count.index)
+    availability_zone = element(var.availability_zone, count.index)
     map_public_ip_on_launch = true
     tags = {
-        name = "public_subnet"
+        name = "public_subnet-${count.index}"
     }
 }
 resource "aws_subnet" "private_subnet"{
     vpc_id = aws_vpc.my_vpc.id
     count = length(var.private_cidr_block)
     cidr_block = element(var.private_cidr_block, count.index)
-    avalability_zone = element(var.avalability_zone, count.index)
+    availability_zone = element(var.availability_zone, count.index)
     
     tags = {
         name = "private_subnet"
@@ -28,10 +28,12 @@ resource "aws_subnet" "private_subnet"{
 }
 
 resource "aws_internet_gateway" "my_gw"{
-vpc_id = aws_vpc.my_vpc.id
-tags = {
-    name = "myGW"
-}
+    vpc_id = aws_vpc.my_vpc.id
+    tags = {
+
+        name = "myGW"
+
+    }
 }
 
 resource "aws_route_table" "publicrt"{
@@ -39,7 +41,7 @@ resource "aws_route_table" "publicrt"{
 
     route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.id
+        gateway_id = aws_internet_gateway.my_gw.id
     }
     tags = {
         name = "public_rt"
@@ -56,13 +58,13 @@ resource "aws_route_table" "privatert"{
 
 resource "aws_route_table_association" "publicass" {
     count = length(var.public_cidr_block)
-    subnet_id = aws_subnet.public[count.index].id
-    route_table_id = aws_route_table.public_rt.id
+    subnet_id = aws_subnet.public_subnet[count.index].id
+    route_table_id = aws_route_table.publicrt.id
 }
 
 resource "aws_route_table_association" "privateass"{
     count = length(var.private_cidr_block)
-    subnet_id = aws_subnet.private[count.index].id
+    subnet_id = aws_subnet.private_subnet[count.index].id
     route_table_id = aws_route_table.privatert.id
     depends_on = [aws_nat_gateway.nat]
 }
@@ -73,15 +75,15 @@ resource "aws_eip" "eip_nat"{
 
 resource "aws_nat_gateway" "nat"{
     allocation_id = aws_eip.eip_nat.id
-    subnet_id = aws_subnet.public[0].id
+    subnet_id = aws_subnet.public_subnet[0].id
     tags = {
         name = "natgatway"
     }
-    depends_on = aws_internet_gateway.my_gw
+    depends_on = [aws_internet_gateway.my_gw]
 }
 resource "aws_route" "nat_route"{
     route_table_id    = aws_route_table.publicrt.id
-    destination_cidr_block = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat.id
+    destination_cidr_block = "0.0.0.0/0"
     depends_on = [aws_route_table.privatert]
 }
