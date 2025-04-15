12a47eebab39a6fa94b7ef5897b90cd158e5aee7
Sam130012 <samyash18012000@gmail.com>
2025-04-15 16:35:04 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -21,7 +21,7 @@ resource "aws_subnet" "private_subnet"{
     count = length(var.private_cidr_block)
     cidr_block = element(var.private_cidr_block, count.index)
     availability_zone = element(var.availability_zone, count.index)
-    
+
     tags = {
         name = "private_subnet"
     }
