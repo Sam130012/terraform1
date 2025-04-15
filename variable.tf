0fe34b7ad7b66d2ec00a3e97100bbf5f1d51b2ef
Sam130012 <samyash18012000@gmail.com>
2025-04-15 16:34:16 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,10 +1,12 @@
 variable "ami_id" {
   type        = string
   default     = "ami-084568db4383264d4"
-  
-}
 
-variable "instane_type" {
+}
+variable "key_name" {
+  default = "main"
+}
+variable "instance_type" {
   type        = string
   default     = "t2.micro"
 }
@@ -17,7 +19,7 @@ variable "private_cidr_block" {
   default     = ["10.0.3.0/24", "10.0.4.0/24"]
   description = "description"
 }
-variable avalability_zone {
+variable availability_zone {
   type        = list(string)
   default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
